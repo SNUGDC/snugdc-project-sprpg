38a2125103404478478cf06e1dc675aa312d87c0
James Kim <eodnjs2998@gmail.com>
2015-06-11 22:02:34 +0900

    ScriptableObjectUtility added.

---
+++ Assets/Gem/Editor/ScriptableObjectUtility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gem
+{
+	public static class ScriptableObjectUtility
+	{
+		public static T CreateAsset<T>(string name = null) where T : ScriptableObject
+		{
+			if (string.IsNullOrEmpty(name))
+				name = "New " + typeof (T).Name;
+			name += ".asset";
+
+			var asset = ScriptableObject.CreateInstance<T>();
+			ProjectWindowUtil.CreateAsset(asset, name);
+			return asset;
+		}
+	}
+}
