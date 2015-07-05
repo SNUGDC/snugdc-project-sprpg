7e27e7ba24d27c59059e5ea311acc9e5d62f1af5
James Kim <eodnjs2998@gmail.com>
2015-07-05 10:42:14 +0900

    HudClock initial commit.

---
+++ Assets/Battle/Hud/HudClock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.Battle
+{
+	public class HudClock : MonoBehaviour
+	{
+		[SerializeField]
+		private Text _text;
+
+		void Update()
+		{
+			var scheduler = HudController.Context.Scheduler;
+			_text.text = scheduler.Relative.ToString();
+			_text.color = scheduler.IsPerfectTerm ? Color.red : Color.black;
+		}
+	}
+}
---
+++ Assets/Battle/Hud/HudController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SPRPG.Battle
+{
+	public class HudController : MonoBehaviour
+	{
+		public static HudController _ { get; private set; }
+		public static Battle Context { get { return _._battleWrapper.Battle; } }
+
+		private BattleWrapper _battleWrapper;
+
+		void Start()
+		{
+			Debug.Assert(_ == null);
+			_ = this;
+
+			_battleWrapper = FindObjectOfType<BattleWrapper>();
+		}
+
+		void OnDestroy()
+		{
+			Debug.Assert(_ == this);
+			_ = null;
+		}
+	}
+}
