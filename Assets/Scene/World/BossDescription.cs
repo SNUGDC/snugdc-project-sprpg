d127c68a5d777827dddf50d7a4021543850bbe28
jayin301 <alswjdghk@snu.ac.kr>
2015-07-18 01:00:32 +0900

    BossDescription implement.

---
+++ Assets/Scene/World/BossDescription.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.World
+{
+	public class BossDescription : MonoBehaviour
+	{
+		public Text Text;
+
+		public void SetDescription(StageId key)
+		{
+			SetDescription(GetDescription(key));
+		}
+
+		private string GetDescription(StageId key)
+		{
+		   return WorldDb.Find(key).Detail;
+		}
+
+		private void SetDescription(string description)
+		{
+			Text.text = description;
+		}
+	}
+}
---
+++ Assets/Scene/World/Editor/BossDescriptionEditor.cs
@@ -0,0 +1,29 @@
+﻿using Gem;
+using UnityEditor;
+using UnityEngine;
+
+namespace SPRPG.World
+{
+	[CustomEditor(typeof(BossDescription))]
+	public class BossDescriptionEditor : ComponentEditor<BossDescription>
+	{
+		private StageId _stageId = StageId.Radiation;
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			if (!Application.isPlaying)
+				return;
+
+			EditorGUILayout.BeginHorizontal();
+
+			_stageId = (StageId)EditorGUILayout.EnumPopup(_stageId);
+
+			if (GUILayout.Button("apply"))
+				Target.SetDescription(_stageId);
+
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+}
