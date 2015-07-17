8d6b99ac0e344480d228d4495e37a3430c2a7117
jayin301 <alswjdghk@snu.ac.kr>
2015-07-18 02:45:38 +0900

    implement BossButton.

---
+++ Assets/Scene/World/BossButton/BossButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.World
+{
+	public class BossButton : MonoBehaviour 
+	{
+		public void SetIcon(BossId id)
+		{
+			var icon = Resources.Load<Sprite>("World/Boss" + id);
+			var image = GetComponent<Image>();
+			image.sprite = icon;
+			image.SetNativeSize();
+		}
+	}
+}
---
+++ Assets/Scene/World/Editor/BossButtonEditor.cs
@@ -0,0 +1,29 @@
+﻿using Gem;
+using UnityEditor;
+using UnityEngine;
+
+namespace SPRPG.World
+{
+	[CustomEditor(typeof(BossButton))]
+	public class BossButtonEditor : ComponentEditor<BossButton>
+	{
+		private BossId _bossId = BossId.Radiation;
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			if (!Application.isPlaying)
+				return;
+
+			EditorGUILayout.BeginHorizontal();
+
+			_bossId = (BossId) EditorGUILayout.EnumPopup(_bossId);
+
+			if (GUILayout.Button("apply"))
+				Target.SetIcon(_bossId);
+
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+}
