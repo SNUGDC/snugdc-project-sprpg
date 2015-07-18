41494b559b4902e7e38418087fdc4ea5110b2a91
James Kim <eodnjs2998@gmail.com>
2015-07-18 14:25:34 +0900

    BossSkillActor: complex properties and readonly fields are replaced by single readonly fields.

--- Assets/Battle/Boss/BossSkillActor.cs
+++ Assets/Battle/Boss/BossSkillActor.cs
@@ -5,20 +5,17 @@ namespace SPRPG.Battle
 {
 	public abstract class BossSkillActor : SkillActorBase
 	{
-		private readonly Battle _context;
-		protected Battle Context { get { return _context; } }
-		private readonly Boss _boss;
-		protected Boss Boss { get { return _boss; } }
-		private readonly BossSkillBalanceData _data;
-		protected BossSkillBalanceData Data { get { return _data; } }
+		protected readonly Battle Context;
+		protected readonly Boss Boss;
+		protected readonly BossSkillBalanceData Data;
 
 		public new Action<BossSkillActor> OnStop;
 
 		protected BossSkillActor(Battle context, Boss boss, BossSkillBalanceData data)
 		{
-			_context = context;
-			_boss = boss;
-			_data = data;
+			Context = context;
+			Boss = boss;
+			Data = data;
 
 			base.OnStop += skillActor => OnStop.CheckAndCall((BossSkillActor)skillActor);
 		}
