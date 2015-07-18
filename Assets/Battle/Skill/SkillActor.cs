2452168cc39853e897da1e6ae87f5bd83f6c9d0a
James Kim <eodnjs2998@gmail.com>
2015-07-18 14:24:55 +0900

    SkillActor: complex properties and readonly fields are replaced by single readonly fields.

--- Assets/Battle/Skill/SkillActor.cs
+++ Assets/Battle/Skill/SkillActor.cs
@@ -5,15 +5,16 @@ namespace SPRPG.Battle
 {
 	public abstract class SkillActor : SkillActorBase
 	{
-		public SkillKey Key { get { return _data.Key; } }
-		public SkillBalanceData Data { get { return _data; } }
-		private readonly SkillBalanceData _data;
+		public SkillKey Key { get { return Data.Key; } }
+		public readonly SkillBalanceData Data;
+		public readonly Character Owner;
 
 		public new Action<SkillActor> OnStop;
 
 		protected SkillActor(SkillBalanceData data)
 		{
-			_data = data;
+			Data = data;
+			Owner = owner;
 			base.OnStop += skillActor => OnStop.CheckAndCall((SkillActor)skillActor);
 		}
 	}
