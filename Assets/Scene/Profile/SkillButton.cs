42a6f8cbba66985d011c4393ebee46ef5c9a28cc
jayin301 <alswjdghk@snu.ac.kr>
2015-07-19 20:03:07 +0900

    add SkillButton to ProfileScene, add "Icon" member variable to skill.json and implement load skill sprite runtime.

---
+++ Assets/Scene/Profile/SkillButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.Profile
+{
+	public class SkillButton : MonoBehaviour
+	{
+		[SerializeField]
+		private Image _icon;
+
+		public void SetIcon(SkillKey key)
+		{
+			var data = SkillBalance._.Find(key);
+			var icon = Resources.Load<Sprite>(data.Icon);
+			_icon.sprite = icon;
+		}
+	}
+}
