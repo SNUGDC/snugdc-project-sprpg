c23f37de49dce2b18d0aba346e63a1dff61d00b8
jayin301 <alswjdghk@snu.ac.kr>
2015-07-19 18:24:46 +0900

    implement CharacterDescription in ProfileScene.

---
+++ Assets/Scene/Profile/CharacterDescription.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.Profile
+{
+	public class CharacterDescription : MonoBehaviour
+	{
+		public Text Text;
+
+		public void SetDescription(string value)
+		{
+			Text.text = value;
+		}
+	}
+}
