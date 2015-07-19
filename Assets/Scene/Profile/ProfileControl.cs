082b078707b412394bd2125abe742d6fa36d82c1
jayin301 <alswjdghk@snu.ac.kr>
2015-07-19 21:36:25 +0900

    implement and set SkillSlots.

--- Assets/Scene/Profile/ProfileControl.cs
+++ Assets/Scene/Profile/ProfileControl.cs
@@ -2,8 +2,19 @@
 
 namespace SPRPG.Profile
 {
-	public class ProfileControl : MonoBehaviour 
+	public class ProfileControl : MonoBehaviour
 	{
+		[SerializeField]
+		private SkillSlots _slots;
+
+		void Start()
+		{
+			_slots.SetSlot(SkillSlot._1, SkillKey.WarriorAttack);
+			_slots.SetSlot(SkillSlot._2, SkillKey.WarriorEvasion);
+			_slots.SetSlot(SkillSlot._3, SkillKey.WarriorHeal);
+			_slots.SetSlot(SkillSlot._4, SkillKey.WarriorStrongAttack);
+		}
+
 		public void Back()
 		{
 			Transition.TransferToCamp();
---
+++ Assets/Scene/Profile/SkillButton/SkillSlots.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SPRPG.Profile
+{
+	public class SkillSlots : MonoBehaviour
+	{
+		[SerializeField] 
+		private List<SkillButton> _slots;
+
+		public void SetSlot(SkillSlot num, SkillKey key)
+		{
+			_slots[num.ToIndex()].SetIcon(key);  
+		}
+	}
+}
