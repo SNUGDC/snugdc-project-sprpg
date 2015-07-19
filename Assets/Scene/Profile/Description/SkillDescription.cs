7cd8371b97cd5c26205a8130555b4c4ffde95038
jayin301 <alswjdghk@snu.ac.kr>
2015-07-19 19:58:43 +0900

    implement "SkillDescription" script to ProfileScene.

---
+++ Assets/Scene/Profile/Description/SkillDescription.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SPRPG
+{
+	
+	public class SkillDescription : MonoBehaviour 
+	{
+		public void SetDescription(SkillKey key)
+		{
+			SkillBalance._.Find(key).Describe();
+		}		
+	}
+}
