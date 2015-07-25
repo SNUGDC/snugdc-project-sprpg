ae6f7037dcd82399e28cb94ca4558e4dc839502b
James Kim <eodnjs2998@gmail.com>
2015-07-25 11:19:00 +0900

    BackButton: added.

---
+++ Assets/Scene/UI/BackButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SPRPG.UI
+{
+	public class BackButton : MonoBehaviour
+	{
+		public void Back()
+		{
+			Transition.TransferToPreviousScene(SceneType.Camp);
+		}
+	}
+}
