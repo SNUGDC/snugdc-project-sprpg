f87f630207a9d516410d57b9326f501ecf1bbea9
James Kim <eodnjs2998@gmail.com>
2015-07-26 14:38:39 +0900

    BattleHelper: BattleHelper logic in Battle is extracted into other file.

---
+++ Assets/Battle/Core/BattleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SPRPG.Battle 
+{
+	public static partial class BattleHelper
+	{
+		public static Job AddPlayerPerform(this Battle thiz, Tick delay, Action callback)
+		{
+			return new Job(thiz.Fsm.PlayerPerform.Schedule, delay, callback);
+		}
+
+		public static Job AddBossPerform(this Battle thiz, Tick delay, Action callback)
+		{
+			return new Job(thiz.Fsm.BossPerform.Schedule, delay, callback);
+		}
+
+		public static Job AddAfterTurn(this Battle thiz, Tick delay, Action callback)
+		{
+			return new Job(thiz.Fsm.AfterTurn.Schedule, delay, callback);
+		}
+	}
+}
