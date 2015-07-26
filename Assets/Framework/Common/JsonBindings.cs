3902537be541c4ffd4f7e0bca1b681d051d54496
James Kim <eodnjs2998@gmail.com>
2015-07-27 02:08:16 +0900

    JsonBindings: access modifier public to private.

--- Assets/Framework/Common/JsonBindings.cs
+++ Assets/Framework/Common/JsonBindings.cs
@@ -5,13 +5,13 @@ namespace SPRPG
 {
 	public static class JsonBindings
 	{
-		public static void BindEnumAsNatural<T>()
+		private static void BindEnumAsNatural<T>()
 		{
 			JsonMapper.RegisterImporter<int, T>(v => (T)(object)v);
 			JsonMapper.RegisterExporter<T>((o, w) => w.Write(Convert.ToInt32(o)));
 		}
 
-		public static void BindEnumAsString<T>()
+		private static void BindEnumAsString<T>()
 		{
 			JsonMapper.RegisterImporter<string, T>(v => (T) Enum.Parse(typeof(T), v));
 			JsonMapper.RegisterExporter<T>((o, w) => w.Write(o.ToString()));
