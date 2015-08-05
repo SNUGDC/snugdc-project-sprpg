a63266a4f8bfe1af425308ee236318eb6c510f72
James Kim <eodnjs2998@gmail.com>
2015-08-06 03:10:21 +0900

    add PauseButton.

---
+++ Assets/Battle/Hud/PauseButton/HudPauseButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Gem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SPRPG.Battle.View
+{
+	public class HudPauseButton : MonoBehaviour
+	{
+		[SerializeField]
+		private Sprite _pauseSprite;
+		[SerializeField]
+		private Sprite _resumeSprite;
+
+		private bool _pauseEnabled;
+		[SerializeField]
+		private Image _image;
+
+		public Action<bool> OnToggle;
+
+		public void Toggle()
+		{
+			ForceSetToggle(!_pauseEnabled);
+			OnToggle.CheckAndCall(_pauseEnabled);
+		}
+
+		public void ForceSetToggle(bool pauseEnabled)
+		{
+			_pauseEnabled = pauseEnabled;
+			var _isNextStatePaused = !_pauseEnabled;
+			_image.sprite = _isNextStatePaused ? _pauseSprite : _resumeSprite;
+		}
+
+		public void OnClick()
+		{
+			Toggle();
+		}
+	}
+}
--- Assets/Battle/ViewController/HudController.cs
+++ Assets/Battle/ViewController/HudController.cs
@@ -11,6 +11,9 @@ namespace SPRPG.Battle.View
 		[SerializeField]
 		private HudClock _clock;
 		[SerializeField]
+		private HudPauseButton _pauseButton;
+
+		[SerializeField]
 		private HudHpBar[] _characterHpBars;
 		[SerializeField]
 		private HudHpBar _bossHpBar;
@@ -20,6 +23,9 @@ namespace SPRPG.Battle.View
 
 		void Start()
 		{
+			_pauseButton.ForceSetToggle(!Context.RealtimeEnabled);
+			_pauseButton.OnToggle += TogglePause;
+
 			_characterHpBarControllers = new HudHpBarController<Character>[SPRPG.Party.Size];
 			foreach (var idx in BattleHelper.GetOriginalPartyIdxEnumerable())
 			{
@@ -31,10 +37,20 @@ namespace SPRPG.Battle.View
 			_bossHpBarController = new HudHpBarController<Boss>(_bossHpBar, Context.Boss);
 		}
 
+		void OnDestroy()
+		{
+			_pauseButton.OnToggle -= TogglePause;
+		}
+
 		void Update()
 		{
 			if (!Context.Fsm.IsResult)
 				_clock.RefreshTime(Context.PlayerClock.Relative);
+		}
+
+		private void TogglePause(bool shouldPause)
+		{
+			Context.RealtimeEnabled = !shouldPause;
 		}
 	}
 }
