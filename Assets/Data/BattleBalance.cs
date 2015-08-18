966f8a51d9d1320ae155857fd5113181b0e252e7
James Kim <eodnjs2998@gmail.com>
2015-08-18 15:12:27 +0900

    move StatusConditionBalance logic to new file.

--- Assets/Data/BattleBalance.cs
+++ Assets/Data/BattleBalance.cs
@@ -1,69 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Gem;
 using LitJson;
 using UnityEngine;
 
 namespace SPRPG.Battle
 {
-	public interface IStatusConditionBalanceData
-	{
-		Tick DefaultDuration { get; set; }
-	}
-
-	public class StatusConditionBalanceData : IStatusConditionBalanceData
-	{
-		[JsonInclude]
-		public Tick DefaultDuration { get; set; }
-	}
-
-	public class StatusConditionPoisonBalanceData : IStatusConditionBalanceData
-	{
-		[JsonInclude]
-		public Tick DefaultDuration { get; set; }
-		public Tick Cooltime;
-		public Hp Damage;
-	}
-
-	public class StatusConditionBossBlindBalanceData : IStatusConditionBalanceData
-	{
-		[JsonInclude]
-		public Tick DefaultDuration { get; set; }
-		public Percentage Accuracy;
-	}
-
-	public class StatusConditionBalanceDatas
-	{
-		private readonly Dictionary<StatusConditionType, IStatusConditionBalanceData> _dict =
-			new Dictionary<StatusConditionType, IStatusConditionBalanceData>();
-
-		public IStatusConditionBalanceData this[StatusConditionType type] { get { return _dict[type]; } }
-
-		public StatusConditionBalanceDatas(Dictionary<string, JsonData> dict, StatusConditionGroup group)
-		{
-			foreach (var kv in dict)
-			{
-				StatusConditionType type;
-				if (EnumHelper.TryParse(kv.Key, out type))
-					_dict[type] = BattleBalanceHelper.CreateStatusCondition(group, type, kv.Value);
-			}
-		}
-	}
-
-	public class CharacterStatusConditionBalanceDatas : StatusConditionBalanceDatas
-	{
-		public CharacterStatusConditionBalanceDatas(Dictionary<string, JsonData> dict) : base(dict, StatusConditionGroup.Character) { }
-		public StatusConditionPoisonBalanceData GetPoison() { return (StatusConditionPoisonBalanceData)this[StatusConditionType.Poison]; }
-	}
-
-	public class BossStatusConditionBalanceDatas : StatusConditionBalanceDatas
-	{
-		public BossStatusConditionBalanceDatas(Dictionary<string, JsonData> dict) : base(dict, StatusConditionGroup.Boss) { }
-		public StatusConditionBalanceData GetFreeze() { return (StatusConditionBalanceData)this[StatusConditionType.Freeze]; }
-		public StatusConditionPoisonBalanceData GetPoison() { return (StatusConditionPoisonBalanceData)this[StatusConditionType.Poison]; }
-		public StatusConditionBossBlindBalanceData GetBlind() { return (StatusConditionBossBlindBalanceData)this[StatusConditionType.Blind]; }
-	}
-
 	public class BattleCharacterBalanceData
 	{
 		public Tick DefaultSkillDuration;
