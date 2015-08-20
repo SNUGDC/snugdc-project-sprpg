e279dc760505259f5ef2f90e9d029fcde9fdb53b
James Kim <eodnjs2998@gmail.com>
2015-08-20 22:44:30 +0900

    add animation controller for paladin.

---
+++ Assets/GO/CharacterInheritance/Paladin/CharacterPaladinView.cs
@@ -0,0 +1,28 @@
+﻿using Gem;
+using UnityEngine;
+
+namespace SPRPG
+{
+	public class CharacterPaladinView : CharacterView 
+	{
+		public GameObject FxMassHeal;
+
+		public override void PlaySkillStart(SkillBalanceData data)
+		{
+			switch (data.Key)
+			{
+				case SkillKey.PaladinMassHeal:
+					PlayMassHeal();
+					return;
+				default:
+					return;
+			}
+		}
+
+		public void PlayMassHeal()
+		{
+			var fx = FxMassHeal.Instantiate();
+			fx.transform.SetParent(FxRoot, false);
+		}
+	}
+}
