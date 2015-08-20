b3364a235b484b98ddfea806d5cfeaba4caa2737
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-21 00:53:54 +0900

    make base outline of archer FX.

--- Assets/GO/CharacterInheritance/Archer/CharacterArcherView.cs
+++ Assets/GO/CharacterInheritance/Archer/CharacterArcherView.cs
@@ -1,10 +1,46 @@
-﻿using UnityEngine;
+﻿using Gem;
+using UnityEngine;
 
 namespace SPRPG
 {
 	public class CharacterArcherView : CharacterView
 	{
-		[SerializeField]
-		private CharacterArcherArrow _arrow;
+		public GameObject FxReload;
+		public GameObject FxEvadeShot;
+		public GameObject FxSnipe;
+		public GameObject FxWeakPointAttack;
+		public GameObject FxArrowRain;
+
+		public override void PlaySkillStart(SkillBalanceData data)
+		{
+			switch (data.Key) 
+			{
+			}
+		}
+
+		public void PlayReload()
+		{
+			InstantiateFx (FxReload);
+		}
+
+		public void PlayEvadeShot()
+		{
+			InstantiateFx (FxEvadeShot);
+		}
+
+		public void PlaySnipe()
+		{
+			InstantiateFx (FxSnipe);
+		}
+
+		public void PlayWeakPointAttack()
+		{
+			InstantiateFx (FxWeakPointAttack);
+		}
+
+		public void PlayArrowRain()
+		{
+			InstantiateFx (FxArrowRain);
+		}
 	}
 }
