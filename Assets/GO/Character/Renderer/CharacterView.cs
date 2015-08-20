a649ea32fb0658e3bbc14e2036ff31327eae5956
James Kim <eodnjs2998@gmail.com>
2015-08-20 21:47:19 +0900

    remove act.

--- Assets/GO/Character/Renderer/CharacterView.cs
+++ Assets/GO/Character/Renderer/CharacterView.cs
@@ -5,23 +5,10 @@ namespace SPRPG
 {
 	public class CharacterView : MonoBehaviour
 	{
-		private class ActContext : Battle.ActContext
-		{
-			private readonly CharacterView _owner;
-			public override Animator Animator { get { return _owner._animator; } }
-			public ActContext(CharacterView owner) { _owner = owner; }
-		}
-
 		[SerializeField]
 		private CharacterSkeleton _skeleton;
 		[SerializeField]
 		private Animator _animator;
-		private ActContext _actContext;
-
-		void Awake()
-		{
-			_actContext = new ActContext(this);
-		}
 
 		public void Attack()
 		{
@@ -35,7 +22,6 @@ namespace SPRPG
 
 		public void PlaySkillStart(SkillBalanceData data)
 		{
-			data.ViewActs.OnStart.CheckAndDo(_actContext);
 		}
 	}
 }
