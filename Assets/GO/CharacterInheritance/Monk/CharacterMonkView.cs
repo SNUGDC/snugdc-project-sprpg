b9a9d9bde1a68dcd178c1c4960424d0454347800
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-21 01:14:18 +0900

    make base work for monk SkillFx.

---
+++ Assets/GO/CharacterInheritance/Monk/CharacterMonkView.cs
@@ -0,0 +1,63 @@
+﻿using Gem;
+using UnityEngine;
+
+namespace SPRPG
+{
+	public class CharacterMonkView : CharacterView
+	{
+		public GameObject FxHeal;
+		public GameObject FxSacrifice;
+		public GameObject FxEquility;
+		public GameObject FxRevenge;
+		public GameObject FxRecovery;
+
+		public override void PlaySkillStart(SkillBalanceData data)
+		{
+			switch (data.Key) 
+			{
+				case(SkillKey.MonkHeal):
+					PlayHeal();
+					return;
+				case(SkillKey.MonkSacrifice):
+					PlaySacrifice();
+					return;
+				case(SkillKey.MonkEquility):
+					PlayEquility();
+					return;
+				case(SkillKey.MonkRevenge):
+					PlayRevenge();
+					return;
+				case(SkillKey.MonkRecovery):
+					PlayRecovery();
+					return;
+				default:
+					return;
+			}
+		}
+
+		public void PlayHeal()
+		{
+			InstantiateFx (FxHeal);
+		}
+
+		public void PlaySacrifice()
+		{
+			InstantiateFx (FxSacrifice);
+		}
+
+		public void PlayEquility()
+		{
+			InstantiateFx (FxEquility);
+		}
+
+		public void PlayRevenge()
+		{
+			InstantiateFx (FxRevenge);
+		}
+
+		public void PlayRecovery()
+		{
+			InstantiateFx (FxRecovery);
+		}
+	}
+}
