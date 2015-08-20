85f51a1e0ed2c14c9f67073a4a692118ca1cd66a
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-20 23:19:55 +0900

    PaladinCharacterView Px Basic work for PM.

--- Assets/GO/CharacterInheritance/Paladin/CharacterPaladinView.cs
+++ Assets/GO/CharacterInheritance/Paladin/CharacterPaladinView.cs
@@ -6,6 +6,11 @@ namespace SPRPG
 	public class CharacterPaladinView : CharacterView 
 	{
 		public GameObject FxMassHeal;
+		public GameObject FxBash;
+		public GameObject FxDefense;
+		public GameObject FxPurification;
+		public GameObject FxBigMassHeal;
+		public GameObject FxMassDefense;
 
 		public override void PlaySkillStart(SkillBalanceData data)
 		{
@@ -14,15 +19,61 @@ namespace SPRPG
 				case SkillKey.PaladinMassHeal:
 					PlayMassHeal();
 					return;
+				case SkillKey.PaladinBash:
+					PlayBash();
+					return;
+				case SkillKey.PaladinDefense:
+					PlayDefense();
+					return;
+				case SkillKey.PaladinPurification:
+					PlayPurification();
+					return;
+				case SkillKey.PaladinBigMassHeal:
+					PlayBigMassHeal();
+					return;
+				case SkillKey.PaladinMassDefense:
+					PlayMassDefense();
+					return;
+
 				default:
 					return;
 			}
 		}
 
+		public void InstantiateFx(GameObject fxPrefab)
+		{
+			var fx = fxPrefab.Instantiate ();
+			fx.transform.SetParent (FxRoot, false);
+		}
+
 		public void PlayMassHeal()
 		{
-			var fx = FxMassHeal.Instantiate();
-			fx.transform.SetParent(FxRoot, false);
+			InstantiateFx (FxMassHeal);
+		}
+
+		public void PlayBash()
+		{
+			InstantiateFx (FxBash);		
+		}
+
+		public void PlayDefense()
+		{
+			InstantiateFx (FxDefense);		
+		}
+
+		public void PlayPurification()
+		{
+			InstantiateFx (FxPurification);
+		}
+
+		public void PlayBigMassHeal()
+		{
+			InstantiateFx (FxBigMassHeal);
+		}
+
+		public void PlayMassDefense()
+		{
+			InstantiateFx (FxMassDefense);
 		}
 	}
 }
