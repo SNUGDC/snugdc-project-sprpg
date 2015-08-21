7953b0463768c9d4a715e2384cb995bedee10e8f
James Kim <eodnjs2998@gmail.com>
2015-08-22 00:10:21 +0900

    remove legacy.

--- Assets/Scene/World/BossDescription/BossDescription.cs
+++ Assets/Scene/World/BossDescription/BossDescription.cs
@@ -6,20 +6,5 @@ namespace SPRPG.World
 	public class BossDescription : MonoBehaviour
 	{
 		public Text Text;
-
-		public void SetDescription(StageId key)
-		{
-			SetDescription(GetDescription(key));
-		}
-
-		private string GetDescription(StageId key)
-		{
-		   return WorldDb.Find(key).Detail;
-		}
-
-		private void SetDescription(string description)
-		{
-			Text.text = description;
-		}
 	}
 }
--- Assets/Scene/World/WorldController.cs
+++ Assets/Scene/World/WorldController.cs
@@ -1,34 +1,9 @@
-﻿using Gem;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SPRPG.World
 {
 	public class WorldController : MonoBehaviour
 	{
-		public BossButton BossButtonPrefab;
-
-		void Start()
-		{
-			CreateAllBossButton();
-		}
-
-		BossButton CreateBossButton()
-		{
-			var button = BossButtonPrefab.Instantiate();
-			button.transform.SetParent(transform, false);
-			return button;
-		}
-
-		void CreateAllBossButton()
-		{
-			foreach (var stageData in WorldDb.GetEnumerable())
-			{
-				var bossButton = CreateBossButton();
-				bossButton.transform.localPosition = stageData.ButtonPosition;
-				bossButton.SetIcon(stageData.Boss);
-			}
-		}
-
 		public void Back()
 		{
 			Transition.TransferToCamp();
@@ -38,11 +13,6 @@ namespace SPRPG.World
 		{
 			// todo: pass stage.
 			Transition.TransferToBattleWithUserBattleDef(StageId.Radiation);
-		}
-
-		public void TransferToSetting()
-		{
-			Transition.TransferToSetting();
 		}
 	}
 }
