01a21b8ad96089c989d70fbc10d9d94e6ead8725
James Kim <eodnjs2998@gmail.com>
2015-08-21 15:19:45 +0900

    fix prefab to be destroyed after time.

---
+++ Assets/Framework/Behaviour/DestroySelf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SPRPG
+{
+	public class DestroySelf : MonoBehaviour
+	{
+		public bool ExecuteOnStart;
+
+		void Start()
+		{
+			if (ExecuteOnStart)
+				Execute();
+		}
+
+		public void Execute()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
