18b817643ed60c6935418cfc15607f1b0c78d914
James Kim <eodnjs2998@gmail.com>
2015-08-21 23:24:19 +0900

    implement SePlayer.

---
+++ Assets/Framework/Behaviour/PlaySe.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SPRPG
+{
+	public class PlaySe : MonoBehaviour
+	{
+		public bool ExecuteOnStart = true;
+		public bool DestroyComponentAfterExecute = true;
+		public AudioClip AudioClip;
+
+		void Start()
+		{
+			if (ExecuteOnStart)
+				Execute();
+		}
+
+		public void Execute()
+		{
+			SePlayer._.Play(AudioClip);
+			if (DestroyComponentAfterExecute)
+				Destroy(this);
+		}
+	}
+}
---
+++ Assets/Framework/Sound/SePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SPRPG
+{
+	public class SePlayer : Singleton<SePlayer>
+	{
+		private readonly List<AudioSource> _playings = new List<AudioSource>();
+
+		private void Update()
+		{
+			_playings.RemoveAll(playing =>
+			{
+				if (playing.isPlaying) return false;
+				Destroy(playing);
+				return true;
+			});
+		}
+
+		public void Play(AudioClip clip)
+		{
+			var audioSource = gameObject.AddComponent<AudioSource>();
+			audioSource.clip = clip;
+			audioSource.Play();
+			_playings.Add(audioSource);
+		}
+	}
+}
