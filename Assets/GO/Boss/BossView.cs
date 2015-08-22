427a311ce3c00eda550f96cef696d4c5773463a0
James Kim <eodnjs2998@gmail.com>
2015-08-22 16:31:57 +0900

    add BossViewEditor.

--- Assets/GO/Boss/BossView.cs
+++ Assets/GO/Boss/BossView.cs
@@ -4,6 +4,13 @@ namespace SPRPG.Battle.View
 {
 	public class BossView : MonoBehaviour
 	{
-		public Animator Animator;
+		[SerializeField]
+		private BossId _id;
+		public BossId Id { get { return _id; } }
+		[SerializeField]
+		private Animator _animator;
+		public Animator Animator { get { return _animator; } }
+
+		public virtual void PlaySkillStart(BossSkillBalanceData data, object arguments) { }
 	}
 }
---
+++ Assets/GO/Boss/Editor/BossViewEditor.cs
@@ -0,0 +1,33 @@
+﻿using Gem;
+using UnityEngine;
+using UnityEditor;
+
+namespace SPRPG.Battle.View
+{
+	[CustomEditor(typeof(BossView))]
+	public class BossViewEditor : ComponentEditor<BossView>
+	{
+		private BossBalanceData _data;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			if (!Application.isPlaying) return;
+			_data = BossBalance._.Find(Target.Id);
+		}
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+			if (!Application.isPlaying) return;
+			foreach (var kv in _data.Skills)
+				RenderSkillButton(kv.Value, null);
+		}
+
+		private void RenderSkillButton(BossSkillBalanceData data, object arguments)
+		{
+			if (GUILayout.Button(data.Key.ToString()))
+				Target.PlaySkillStart(data, null);
+		}
+	}
+}
