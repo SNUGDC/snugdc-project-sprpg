dc06ebfada5e60ceed948a756fdc69cfac85a82e
James Kim <eodnjs2998@gmail.com>
2015-08-22 11:20:57 +0900

    fix immediate starting battle.

--- Assets/Battle/Test/BattleTest.cs
+++ Assets/Battle/Test/BattleTest.cs
@@ -1,19 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SPRPG.Battle
 {
+	[Serializable]
+	public struct TestPartyDef
+	{
+		public CharacterId _1;
+		public CharacterId _2;
+		public CharacterId _3;
+
+		private static CharacterDef MakeCharacterDef(CharacterBalanceData data)
+		{
+			return new CharacterDef(data, data.SkillSetDefault);
+		}
+
+		public PartyDef Build()
+		{
+			var balance = CharacterBalance._;
+			return new PartyDef(
+				MakeCharacterDef(balance.Find(_1)), 
+				MakeCharacterDef(balance.Find(_2)), 
+				MakeCharacterDef(balance.Find(_3)));
+		}
+	}
+
 	public class BattleTest : MonoBehaviour
 	{
 		public StageId Stage;
 		public bool RealtimeEnabled;
-		public PartyDef Party;
+		public TestPartyDef Party;
 
-		void Awake()
+		void Start()
 		{
 			if (BattleWrapper.Def != null)
 				return;
 
-			var def = new BattleDef(Stage, Party)
+			var def = new BattleDef(Stage, Party.Build())
 			{
 #if BALANCE
 				UseDataInput = false,
