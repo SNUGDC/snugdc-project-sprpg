18c8e17ab402c3ce4297bfea127b37f4866c3e8f
James Kim <eodnjs2998@gmail.com>
2015-08-22 10:41:19 +0900

    renew BossButton.

---
+++ Assets/Scene/World/BossButton/BossButton.cs
@@ -0,0 +1,21 @@
+﻿using Gem;
+using System;
+using UnityEngine;
+
+namespace SPRPG.World
+{
+	public class BossButton : MonoBehaviour
+	{
+		private bool _isSelected;
+		[SerializeField]
+		private Animator _animator;
+		public Action<BossButton, bool> OnClickCallback;
+
+		public void OnClick()
+		{
+			_isSelected = !_isSelected;
+			_animator.SetTrigger(_isSelected ? "Select" : "Deselect");
+			OnClickCallback.CheckAndCall(this, _isSelected);
+		}
+	}
+}
